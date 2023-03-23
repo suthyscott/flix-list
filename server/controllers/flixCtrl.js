4e074905e73ccc90efc0d6eaeabda7b2d7e0dc0d
suthyscott <sutherlandscott21@gmail.com>
2023-03-23 09:59:21 -0600

    built out addMovie handler function

--- server/controllers/flixCtrl.js
+++ server/controllers/flixCtrl.js
@@ -1,8 +1,23 @@
-
+const {Movie, Show} = require("../models/tables")
 
 module.exports = {
     addMovie: async (req, res) => {
-        // receive necessary info on req.body, insert it into the db, send back a response
+       try{
+        const {userId, movieName, priority, imageUrl, length} = req.body
+
+        await Movie.create({
+            userId,
+            movieName,
+            priority,
+            imageUrl,
+            length
+        })
+
+        res.sendStatus(200)
+       } catch(err){
+        console.log(err)
+        res.status(500).send('The movie was not added correctly!')
+       }
     },
     addShows: async (req, res) => {
         // receive necessary info on req.body, insert it into the db, send back a response
